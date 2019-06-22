8dd747f0e7a2e5ab4cd64eb4cf6c5c2f6582e447
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-06-22 10:05:27 +0900

    Create handlers for api

---
+++ src/server/handlers.tsx
@@ -0,0 +1,45 @@
+import express from 'express';
+
+import { Resource } from 'lib/Resource';
+
+export function fetchResources(req: express.Request, res: express.Response) {
+  const query = req.query;
+  const conditions = {};
+  const options = {};
+
+  const conditionWhiteList = ['id', 'type'];
+  for (let key of conditionWhiteList) {
+    if (query[key]) {
+      conditions[key] = query[key].split(',');
+    }
+  }
+  const optionWhiteList = ['locale', 'limit', 'offset', 'sort'];
+  for (let key of optionWhiteList) {
+    if (options[key]) {
+      options[key] = query[key].split(',');
+    }
+  }
+
+  const resources = Resource.find(conditions, options);
+  res.json(resources);
+}
+
+export function createResource(req: express.Request, res: express.Response) {
+  res.json({ a: 2 });
+}
+
+export function fetchResource(req: express.Request, res: express.Response) {
+  const params = req.params;
+  const query = req.query;
+
+  const resource = Resource.find({ id: params.id }, { locale: query.locale });
+  res.json(resource);
+}
+
+export function updateResource(req: express.Request, res: express.Response) {
+  res.json({ a: 2 });
+}
+
+export function deleteResource(req: express.Request, res: express.Response) {
+  res.json({ a: 2 });
+}
