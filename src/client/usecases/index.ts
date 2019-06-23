15f377e00ba44ae29269db4365b6b4408b3039b5
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-06-23 21:38:25 +0900

    Remove unused usecases

--- src/client/usecases/index.ts
+++ src/client/usecases/index.ts
@@ -4,28 +4,6 @@ import { ThunkDispatch } from 'redux-thunk';
 import { fetchingCount, fetchedCount, increment, decrement } from 'client/actions';
 import { ExampleAPI } from 'client/services';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const fetchResources = (params: any) => {
-  return (dispatch: ThunkDispatch<{}, {}, Action>) => {
-    dispatch(fetchingCount());
-    return ExampleAPI.call().then(() => {
-      dispatch(increment());
-      dispatch(fetchedCount());
-    });
-  };
-};
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const fetchResource = (params: any) => {
-  return (dispatch: ThunkDispatch<{}, {}, Action>) => {
-    dispatch(fetchingCount());
-    return ExampleAPI.call().then(() => {
-      dispatch(increment());
-      dispatch(fetchedCount());
-    });
-  };
-};
-
 export const asyncIncrement = () => {
   return (dispatch: ThunkDispatch<{}, {}, Action>) => {
     dispatch(fetchingCount());
