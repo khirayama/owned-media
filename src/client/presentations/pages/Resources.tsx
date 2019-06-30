2d8546406632b07f1e668656bc48d138eed4899d
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-07-01 07:38:04 +0900

    Update Resources page

--- src/client/presentations/pages/Resources.tsx
+++ src/client/presentations/pages/Resources.tsx
@@ -1,41 +1,9 @@
 import * as React from 'react';
-import styled from 'styled-components';
 
 import { ResourceList } from 'lib/components/ResourceList';
 import { Props as ResourceListItemProps } from 'lib/components/ResourceListItem';
 
 import { config } from 'config';
-
-export const Wrapper = styled.div`
-  padding: 24px 16px;
-
-  h2 {
-    margin-bottom: 36px;
-  }
-
-  button + table {
-    margin-top: 24px;
-  }
-
-  tr {
-    th:nth-of-type(5n + 1) {
-      width: 20px;
-    }
-    th:nth-of-type(5n + 2) {
-      width: 160px;
-    }
-    th:nth-of-type(5n + 4) {
-      width: 60px;
-    }
-    th:nth-of-type(5n) {
-      width: 132px;
-    }
-  }
-
-  button + button {
-    margin-left: 12px;
-  }
-`;
 
 export class Resources extends React.Component<any, any> {
   public render() {
@@ -55,14 +23,11 @@ export class Resources extends React.Component<any, any> {
     };
 
     return (
-      <Wrapper>
-        <h2>Resources</h2>
-        <ResourceList
-          onClickNewResourceButton={onClickNewResourceButton}
-          onClickEditResourceButton={onClickEditResourceButton}
-          onClickDeleteResourceButton={onClickDeleteResourceButton}
-        />
-      </Wrapper>
+      <ResourceList
+        onClickNewResourceButton={onClickNewResourceButton}
+        onClickEditResourceButton={onClickEditResourceButton}
+        onClickDeleteResourceButton={onClickDeleteResourceButton}
+      />
     );
   }
 }
