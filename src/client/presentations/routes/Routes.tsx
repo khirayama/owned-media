472702cf9576d0876c141389c34b2f5033775c7f
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-06-30 10:03:48 +0900

    Update Routes

--- src/client/presentations/routes/Routes.tsx
+++ src/client/presentations/routes/Routes.tsx
@@ -3,41 +3,10 @@ import { Route, Switch } from 'react-router-dom';
 
 import { Home } from 'client/presentations/pages/Home';
 import { About } from 'client/presentations/pages/About';
-import { ResourceList } from 'lib/components/ResourceList';
-import { ResourceForm } from 'lib/components/ResourceForm';
-import { Props as ResourceListItemProps } from 'lib/components/ResourceListItem';
+import { Resources } from 'client/presentations/pages/Resources';
+import { Resource } from 'client/presentations/pages/Resource';
 
 import { config } from 'config';
-
-class Resources extends React.Component<any, any> {
-  public render() {
-    const onClickNewResourceButton = () => {
-      this.props.history.push(`${config.path.admin}/resources/new`);
-    };
-
-    const onClickEditResourceButton = (event: React.MouseEvent<HTMLButtonElement>, props: ResourceListItemProps) => {
-      this.props.history.push(`${config.path.admin}/resources/${props.resource.id}`);
-    };
-
-    return (
-      <ResourceList
-        onClickNewResourceButton={onClickNewResourceButton}
-        onClickEditResourceButton={onClickEditResourceButton}
-      />
-    );
-  }
-}
-
-class Resource extends React.Component<any, any> {
-  public render() {
-    const id = this.props.match.params.id || null;
-    const onClickResourcesLink = () => {
-      this.props.history.push(`${config.path.admin}/resources`);
-    };
-
-    return <ResourceForm resourceId={id} onClickResourcesLink={onClickResourcesLink} />;
-  }
-}
 
 export const routes = [
   {
