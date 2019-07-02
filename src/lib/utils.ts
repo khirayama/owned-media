9db7235b8a95d2f3aa2daa98a3514b4ccaa2a814
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-07-02 21:10:47 +0900

    Add some functions to utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/camelcase */
+import { ResourceFullShape, ResourceShape, ResourceRequest, ResourceFullResponse, ResourceResponse } from 'lib/types';
+
 export function createLocaleObj(locales: string[]) {
   const obj = {};
   for (let locale of locales) {
@@ -26,4 +29,107 @@ export function mergeDeep(target: any, ...sources: any): any {
   }
 
   return mergeDeep(target, ...sources);
+}
+
+export function snakeToCamel(t: string) {
+  return t.replace(/_./g, (s: string) => s.charAt(1).toUpperCase());
+}
+
+export function camelToSnake(t: string) {
+  return t.replace(/([A-Z])/g, (s: string) => '_' + s.charAt(0).toLowerCase());
+}
+
+export function resourceToRequest(resource: ResourceShape): ResourceRequest {
+  return {
+    type: resource.type,
+    key: resource.key,
+    name: resource.name,
+    image_url: resource.imageUrl,
+    page: {
+      title: resource.page.title,
+      description: resource.page.description,
+      image_url: resource.page.imageUrl,
+      keywords: resource.page.keywords,
+    },
+    attributes: resource.attributes,
+  };
+}
+
+export function resourceFullToRequest(resourceFull: ResourceFullShape, locale: string): ResourceRequest {
+  return {
+    type: resourceFull.type,
+    key: resourceFull.key,
+    name: resourceFull.name[locale],
+    image_url: resourceFull.imageUrl[locale],
+    page: {
+      title: resourceFull.page.title[locale],
+      description: resourceFull.page.description[locale],
+      image_url: resourceFull.page.imageUrl[locale],
+      keywords: resourceFull.page.keywords[locale],
+    },
+    attributes: resourceFull.attributes,
+  };
+}
+
+export function responseToResource(resourceResponse: ResourceResponse): ResourceShape {
+  return {
+    id: resourceResponse.id,
+    type: resourceResponse.type,
+    key: resourceResponse.key,
+    name: resourceResponse.name,
+    imageUrl: resourceResponse.image_url,
+    bodyPath: resourceResponse.body_path,
+    body: resourceResponse.body,
+    page: {
+      title: resourceResponse.page.title,
+      description: resourceResponse.page.description,
+      keywords: resourceResponse.page.keywords,
+      imageUrl: resourceResponse.page.image_url,
+    },
+    attributes: resourceResponse.attributes,
+    createdAt: resourceResponse.created_at,
+    updatedAt: resourceResponse.updated_at,
+  };
+}
+
+export function responseFullToResourceFull(resourceFullResponse: ResourceFullResponse): ResourceFullShape {
+  return {
+    id: resourceFullResponse.id,
+    type: resourceFullResponse.type,
+    key: resourceFullResponse.key,
+    name: resourceFullResponse.name,
+    imageUrl: resourceFullResponse.image_url,
+    bodyPath: resourceFullResponse.body_path,
+    body: resourceFullResponse.body,
+    page: {
+      title: resourceFullResponse.page.title,
+      description: resourceFullResponse.page.description,
+      keywords: resourceFullResponse.page.keywords,
+      imageUrl: resourceFullResponse.page.image_url,
+    },
+    attributes: resourceFullResponse.attributes,
+    createdAt: resourceFullResponse.created_at,
+    updatedAt: resourceFullResponse.updated_at,
+  };
+}
+
+export function requestToResource(req: ResourceRequest): ResourceShape {
+  return {
+    id: '',
+    type: req.type,
+    name: req.name,
+    key: req.key,
+    imageUrl: req.image_url,
+    bodyPath: req.body_path || '',
+    body: '',
+    page: {
+      title: req.page.title,
+      description: req.page.description,
+      keywords: req.page.keywords,
+      imageUrl: req.page.image_url,
+    },
+    attributes: req.attributes,
+    createdAt: '',
+    updatedAt: '',
+  };
 }
