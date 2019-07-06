dcdac9a249c35fa69528d36475827a0c8385cfac
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-07-07 07:05:20 +0900

    Update pages

--- publisher/src/client/presentations/pages/Resources.tsx
+++ publisher/src/client/presentations/pages/Resources.tsx
@@ -1,24 +1,13 @@
 import * as React from 'react';
 
-import { Resource as ResourceService } from '../../services/Resource';
-import { ResourceList } from '../components/ResourceList';
-import { Props as ResourceListItemProps } from '../components/ResourceListItem';
+import { ResourceList } from '../../containers/ResourceList';
 import { FlatLink } from '../components/Button';
 
-export class Resources extends React.Component<any, any> {
-  public render() {
-    const onClickDeleteResourceButton = (event: React.MouseEvent<HTMLButtonElement>, props: ResourceListItemProps) => {
-      const isDelete = window.confirm('Delete this resource?');
-      if (isDelete) {
-        ResourceService.delete(props.resource.id);
-      }
-    };
-
-    return (
-      <>
-        <FlatLink to="/resources/new">CREATE NEW RESOURCE</FlatLink>
-        <ResourceList onClickDeleteResourceButton={onClickDeleteResourceButton} />
-      </>
-    );
-  }
+export function Resources() {
+  return (
+    <>
+      <FlatLink to="/resources/new">CREATE NEW RESOURCE</FlatLink>
+      <ResourceList />
+    </>
+  );
 }
