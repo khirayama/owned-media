673b615682a14886631e775dc0657fd0313baf08
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-07-06 14:59:06 +0900

    Update actions

--- publisher/src/client/actions/index.ts
+++ publisher/src/client/actions/index.ts
@@ -1,5 +1,52 @@
 import { Action } from 'redux';
 
+import { ResourceShape } from '../../types';
+
+export interface ChangeIsFetchingResources extends Action<'CHANGE_IS_FETCHING_RESOURCES'> {
+  payload: {
+    isFetching: boolean;
+  };
+}
+
+export const changeIsFetchingResources = (isFetching: boolean): ChangeIsFetchingResources => {
+  return {
+    type: 'CHANGE_IS_FETCHING_RESOURCES',
+    payload: {
+      isFetching,
+    },
+  };
+};
+
+export interface SetResources extends Action<'SET_RESOURCES'> {
+  payload: {
+    resources: ResourceShape[];
+  };
+}
+
+export const setResources = (resources: ResourceShape[]): SetResources => {
+  return {
+    type: 'SET_RESOURCES',
+    payload: {
+      resources,
+    },
+  };
+};
+
+export interface RemoveResource extends Action<'REMOVE_RESOURCE'> {
+  payload: {
+    resourceId: string;
+  };
+}
+
+export const removeResource = (resourceId: string): RemoveResource => {
+  return {
+    type: 'REMOVE_RESOURCE',
+    payload: {
+      resourceId,
+    },
+  };
+};
+
 export interface ChangeLocale extends Action<'CHANGE_LOCALE'> {
   payload: {
     locale: string;
