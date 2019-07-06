477c14490853e7914e1da71b7462b91ae1b38df5
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-07-07 07:04:33 +0900

    Fix api path

--- publisher/src/client/services/Resource.ts
+++ publisher/src/client/services/Resource.ts
@@ -1,17 +1,13 @@
 import axios from 'axios';
-import * as path from 'path';
 
 import { ResourceShape } from '../../types';
 import { resourceToRequest } from '../../utils';
-
-const CONFIG_PATH = path.join(process.cwd(), 'config');
-const { config } = require(CONFIG_PATH);
 
 export class Resource {
   public static fetch(options?: { locale: string }) {
     return new Promise((resolve: (res: ResourceShape[]) => void) => {
       const locale = options ? options.locale : null;
-      const url = `${config.path.api}/resources` + (locale ? `?locale=${locale}` : '');
+      const url = `/api/resources` + (locale ? `?locale=${locale}` : '');
 
       axios.get(url).then(res => {
         resolve(res.data);
@@ -21,7 +17,7 @@ export class Resource {
 
   public static find(resourceId: string, options?: { locale: string }) {
     const locale = options ? options.locale : null;
-    const url = `${config.path.api}/resources/${resourceId}` + (locale ? `?locale=${locale}` : '');
+    const url = `/api/resources/${resourceId}` + (locale ? `?locale=${locale}` : '');
 
     return new Promise((resolve: (res: ResourceShape) => void) => {
       axios.get(url).then(res => {
@@ -32,7 +28,7 @@ export class Resource {
 
   public static create(resource: ResourceShape, options?: { locale: string }): Promise<ResourceShape> {
     const locale = options ? options.locale : null;
-    const url = `${config.path.adminApi}/resources` + (locale ? `?locale=${locale}` : '');
+    const url = `/api/resources` + (locale ? `?locale=${locale}` : '');
 
     return new Promise((resolve: (res: ResourceShape) => void) => {
       axios.post(url, resourceToRequest(resource)).then(res => {
@@ -47,7 +43,7 @@ export class Resource {
     options?: { locale: string },
   ): Promise<ResourceShape> {
     const locale = options ? options.locale : null;
-    const url = `${config.path.adminApi}/resources/${resourceId}` + (locale ? `?locale=${locale}` : '');
+    const url = `/api/resources/${resourceId}` + (locale ? `?locale=${locale}` : '');
 
     return new Promise((resolve: (res: ResourceShape) => void) => {
       axios.put(url, resourceToRequest(resource)).then(res => {
@@ -57,7 +53,7 @@ export class Resource {
   }
 
   public static delete(resourceId: string): Promise<ResourceShape> {
-    const url = `${config.path.adminApi}/resources/${resourceId}`;
+    const url = `/api/resources/${resourceId}`;
 
     return new Promise((resolve: (res: ResourceShape) => void) => {
       axios.delete(url).then(res => {
