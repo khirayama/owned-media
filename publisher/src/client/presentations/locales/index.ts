b98a3ae2da7053727d3df3103ca72928f6a58cfd
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-07-08 07:15:16 +0900

    Update locales

--- publisher/src/client/presentations/locales/index.ts
+++ publisher/src/client/presentations/locales/index.ts
@@ -5,16 +5,29 @@ import jaLocaleData from 'react-intl/locale-data/ja';
 addLocaleData(enLocaleData);
 addLocaleData(jaLocaleData);
 
+export const supportLocales = [
+  {
+    value: 'en',
+    display: 'English',
+  },
+  {
+    value: 'ja',
+    display: '日本語',
+  },
+];
+
 export const en = {
   'Resources.Title': 'Resources',
-  'Resources.Description': 'Resources Page',
+  'Resources.Description': 'Resources',
+  'Resources.Heading': 'Resources',
   'Resource.Title': 'Resource',
   'Resource.Description': 'Resource Page',
 };
 
 export const ja = {
-  'Resources.Title': 'リソース',
-  'Resources.Description': 'リソースページ',
+  'Resources.Title': 'リソース一覧',
+  'Resources.Description': 'リソース一覧',
+  'Resources.Heading': 'リソース一覧',
   'Resource.Title': 'リソース',
   'Resource.Description': 'リソースページ',
 };
