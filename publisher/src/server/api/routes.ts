07699e7cdc3eb5faad0397ae15cea8197bed90b4
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-07-10 07:24:31 +0900

    Add a routing

--- publisher/src/server/api/routes.ts
+++ publisher/src/server/api/routes.ts
@@ -1,11 +1,19 @@
 import express from 'express';
 
-import { fetchResources, fetchResource, createResource, updateResource, deleteResource } from './handlers';
+import {
+  fetchResources,
+  fetchResource,
+  createResource,
+  updateResource,
+  deleteResource,
+  fetchRelatedResources,
+} from './handlers';
 
 export const api: express.Application = express();
 api
   .get('/resources', fetchResources)
   .get('/resources/:id', fetchResource)
+  .get('/resources/:id/relations', fetchRelatedResources)
   .post('/resources', createResource)
   .put('/resources/:id', updateResource)
   .delete('/resources/:id', deleteResource);
