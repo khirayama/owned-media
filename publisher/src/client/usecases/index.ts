b417563062fcd67a72b3bba4b7161ba832eebf4e
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-07-11 09:40:55 +0900

    Implement createResource

--- publisher/src/client/usecases/index.ts
+++ publisher/src/client/usecases/index.ts
@@ -10,6 +10,45 @@ import {
   removeResource,
 } from '../actions';
 import { Resource as ResourceService } from '../services/Resource';
+import {
+  loadConfig,
+  resourceWithAllLocalesToResource,
+  mergeDeep,
+  resourceToPartialResourceWithAllLocales,
+} from '../../utils';
+
+const config = loadConfig();
+
+export const createResource = (resource: ResourceWithAllLocalesShape) => {
+  return (dispatch: ThunkDispatch<{}, {}, Action>) => {
+    dispatch(changeIsFetchingResources(true));
+
+    const firstLocale = config.locales[0];
+    const otherLocales = config.locales.slice(1, config.locales.length);
+    ResourceService.create(resourceWithAllLocalesToResource(resource, firstLocale), { locale: firstLocale }).then(
+      (newResource: ResourceShape) => {
+        dispatch(
+          setResource(mergeDeep({}, resource, resourceToPartialResourceWithAllLocales(newResource, firstLocale))),
+        );
+        Promise.all(
+          otherLocales.map((locale: string) => {
+            return ResourceService.update(newResource.id, resourceWithAllLocalesToResource(resource, locale), {
+              locale,
+            });
+          }),
+        ).then(res => {
+          for (let i = 0; i < otherLocales.length; i += 1) {
+            const locale = otherLocales[i];
+            const tmpResource: ResourceShape = res[i] as ResourceShape;
+            dispatch(
+              setResource(mergeDeep({}, resource, resourceToPartialResourceWithAllLocales(tmpResource, locale))),
+            );
+          }
+        });
+      },
+    );
+  };
+};
 
 export const fetchResources = (options?) => {
   return (dispatch: ThunkDispatch<{}, {}, Action>) => {
