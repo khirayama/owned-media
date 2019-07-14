73583c949e571813ff146be66557fb55c3e750ca
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-07-14 14:05:49 +0900

    Create renderFullPage

---
+++ green-handbook/src/renderFullPage.ts
@@ -0,0 +1,35 @@
+type Params = {
+  locale: string;
+  meta: string;
+  assets: Array<string>;
+  body: string;
+  style: string;
+  preloadedState: string;
+  beforeClosingBodyContent: string;
+};
+
+const escapeHTML = (str: string) => {
+  return str
+    .replace(/&/g, '&amp;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#39;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;');
+};
+
+export const renderFullPage = ({ locale, meta, assets, body, style, preloadedState, beforeClosingBodyContent }: Params) => {
+  return `<!DOCTYPE html>
+    <html lang=${locale}>
+      <head>
+        ${meta}
+        ${style}
+        ${assets.map(asset => `<script src=${asset} defer></script>`).join('\n')}
+      </head>
+      <body>
+        <div id="root">${body}</div>
+        <script id="initial-data" type="text/plain" data-json="${escapeHTML(preloadedState)}"></script>
+        ${beforeClosingBodyContent}
+      </body>
+    </html>
+  `.trim();
+};
