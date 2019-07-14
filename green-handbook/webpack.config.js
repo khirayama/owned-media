8b943c4f465d3720e815e7bd5d5310f76873cf1c
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-07-14 12:47:08 +0900

    Create webpack.config

---
+++ green-handbook/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+const webpack = require('webpack');
+
+const publisherConfig = require('./config');
+const generateHtmlWebpackPlugins = require('./helpers/generateHtmlWebpackPlugins');
+const pagePath = path.resolve(process.cwd(), 'src', 'index.ts');
+
+module.exports = (env, argv) => {
+  const config = {
+    entry: {
+      index: './src/index.ts',
+    },
+    output: {
+      filename: '[name].[hash].js',
+      publicPath: '/',
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.json'],
+    },
+    plugins: generateHtmlWebpackPlugins(publisherConfig),
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: {
+            loader: 'ts-loader',
+            options: {
+              configFile: 'tsconfig.json',
+            },
+          },
+        },
+      ],
+    },
+  };
+
+  return config;
+};
