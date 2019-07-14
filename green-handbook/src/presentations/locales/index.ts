938f102870fb5e4e14636ac55de0bc5ec0e3c915
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-07-14 17:28:29 +0900

    Update locales

--- green-handbook/src/presentations/locales/index.ts
+++ green-handbook/src/presentations/locales/index.ts
@@ -5,6 +5,47 @@ import jaLocaleData from 'react-intl/locale-data/ja';
 addLocaleData(enLocaleData);
 addLocaleData(jaLocaleData);
 
+const dic = {
+  Home: {
+    Title: {
+      ja: 'やぁ、ホーム！',
+      en: 'Hi, Home!',
+    },
+    Description: {
+      ja: 'ここはホームページです。',
+      en: 'This is Home page!',
+    },
+  },
+  About: {
+    Title: {
+      ja: 'やぁ、アバウト',
+      en: 'Hi, About!',
+    },
+    Description: {
+      ja: 'ここはアバウトページです',
+      en: 'This is About page!',
+    },
+  },
+  Users: {
+    Title: {
+      ja: 'やぁ、ユーザーズ',
+      en: 'Hi, Users!',
+    },
+    Description: {
+      ja: 'ここはユーザーズページです',
+      en: 'This is Users page!',
+    },
+  },
+  Counter: {
+    Label: {
+      ja: '{name}さんのカウント数: ',
+      en: 'Counted by {name}: ',
+    },
+  },
+};
+
+/********************
+// Example
 export const en = {
   'Home.Title': 'Hi, Home!',
   'Home.Description': 'This is Home page!',
@@ -14,7 +55,6 @@ export const en = {
   'Users.Description': 'This is Users page!',
   'Counter.Label': 'Counted by {name}: ',
 };
-
 export const ja = {
   'Home.Title': 'やぁ、ホーム！',
   'Home.Description': 'ここはホームページです。',
@@ -24,6 +64,24 @@ export const ja = {
   'Users.Description': 'ここはユーザーズページです',
   'Counter.Label': '{name}さんのカウント数: ',
 };
+********************/
+function generateLocals(data, locale, result = {}, prefix?) {
+  if (typeof data === 'object') {
+    const keys = Object.keys(data);
+    for (let key of keys) {
+      const value = data[key];
+      if (typeof value === 'object') {
+        generateLocals(value, locale, result, prefix ? [prefix, key].join('.') : key);
+      } else if (typeof value === 'string') {
+        result[prefix] = value;
+      }
+    }
+  }
+  return result;
+}
+
+const en = generateLocals(dic, 'en');
+const ja = generateLocals(dic, 'ja');
 
 export function chooseLocale(locale: string) {
   switch (locale) {
