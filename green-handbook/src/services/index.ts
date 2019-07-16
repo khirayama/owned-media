5bad7921022e40925562be62c52b9cac21592862
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-07-17 08:30:47 +0900

    Create services

--- green-handbook/src/services/index.ts
+++ green-handbook/src/services/index.ts
@@ -1,9 +1,18 @@
-export class ExampleAPI {
-  static call() {
-    return new Promise(resolve => {
-      setTimeout(() => {
-        resolve();
-      }, 3000);
+import axios, { AxiosResponse } from 'axios';
+
+const PORT = process.env.PORT || 3000;
+
+const req =
+  typeof window === 'object'
+    ? axios.create()
+    : axios.create({
+        baseURL: `http://localhost:${PORT}`,
+      });
+
+export class Resource {
+  public static find(key: string, locale: string) {
+    return req.get(`/${locale}/resources/${key}.json`).then((res: AxiosResponse) => {
+      return res.data;
     });
   }
 }
