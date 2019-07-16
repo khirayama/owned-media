2c10c5e5110252c6b88df70f6d75e83e49066139
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-07-17 07:01:47 +0900

    Update dev server

--- green-handbook/cli/dev.ts
+++ green-handbook/cli/dev.ts
@@ -2,11 +2,26 @@ import express from 'express';
 
 import { renderer } from '../src/renderer/renderer';
 
+// eslint-disable-next-line node/no-missing-require, node/no-extraneous-require
+const { Resource } = require('publisher');
+
 const PORT = 3000;
 
 const app = express();
 
+Resource.init();
+
 app.use('/', express.static('dist'));
+
+app
+  .get('/:locale/resources.json', (req: express.Request, res: express.Response) => {
+    const resources = Resource.find(null, { locale: req.params.locale });
+    res.json(resources);
+  })
+  .get('/:locale/resources/:key.json', (req: express.Request, res: express.Response) => {
+    const resource = Resource.find({ key: [req.params.key] }, { locale: req.params.locale });
+    res.json(resource);
+  });
 
 app.get('*', (req: express.Request, res: express.Response) => {
   renderer(req.url).then((fullPageHTML: string) => {
