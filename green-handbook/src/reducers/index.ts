d8d184d14e01dae7fc5b5937fb833ef15d0e8cb6
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-07-17 08:30:34 +0900

    Update reducer and shape of state

--- green-handbook/src/reducers/index.ts
+++ green-handbook/src/reducers/index.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line node/no-missing-import, node/no-extraneous-import
 import { ResourceShape } from 'publisher';
 
 declare var window: any;
@@ -20,13 +21,22 @@ export const initialState: State = {
 
 export function reducer(state = initialState, action: any) {
   const newState = { ...state };
+  const payload = action.payload;
 
   switch (action.type) {
+    case 'SET_RESOURCE': {
+      const resource = payload.resource;
+      newState.resources[resource.key || resource.id] = resource;
+      break;
+    }
     case 'CHANGE_LOCALE': {
-      newState.ui.locale = action.payload.locale;
+      newState.ui.locale = payload.locale;
       break;
     }
     default:
   }
+  console.log('state', state);
+  console.log('action', action);
+  console.log('newState', newState);
   return newState;
 }
