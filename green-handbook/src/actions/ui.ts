e33d49aad0e818d3d6fc6ee8ad173e6d151de71c
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-07-18 07:33:12 +0900

    Create actions/ui

---
+++ green-handbook/src/actions/ui.ts
@@ -0,0 +1,18 @@
+import { Action } from 'redux';
+
+export type Actions = ChangeLocale;
+
+export interface ChangeLocale extends Action<'CHANGE_LOCALE'> {
+  payload: {
+    locale: string;
+  };
+}
+
+export const changeLocale = (locale: string): ChangeLocale => {
+  return {
+    type: 'CHANGE_LOCALE',
+    payload: {
+      locale,
+    },
+  };
+};
