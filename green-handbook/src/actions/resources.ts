849cd1170654f5656ae559fb7fa74d184ebdf987
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-07-18 07:33:04 +0900

    Create actions/resources

---
+++ green-handbook/src/actions/resources.ts
@@ -0,0 +1,46 @@
+import { Action } from 'redux';
+
+// eslint-disable-next-line node/no-missing-import, node/no-extraneous-import
+import { ResourceShape } from 'publisher';
+
+export type Actions = SetResource | ChangeIsFetchingResource | ResetResource;
+
+export interface SetResource extends Action<'SET_RESOURCE'> {
+  payload: {
+    resource: ResourceShape | null;
+  };
+}
+
+export const setResource = (resource): SetResource => {
+  return {
+    type: 'SET_RESOURCE',
+    payload: {
+      resource,
+    },
+  };
+};
+
+export interface ChangeIsFetchingResource extends Action<'CHANGE_IS_FETCHING_RESOURCE'> {
+  payload: {
+    resourceId: string;
+    isFetching: boolean;
+  };
+}
+
+export const changeIsFetchingResource = (resourceId: string, isFetching: boolean): ChangeIsFetchingResource => {
+  return {
+    type: 'CHANGE_IS_FETCHING_RESOURCE',
+    payload: {
+      resourceId,
+      isFetching,
+    },
+  };
+};
+
+export interface ResetResource extends Action<'RESET_RESOURCE'> {}
+
+export const resetResource = (): ResetResource => {
+  return {
+    type: 'RESET_RESOURCE',
+  };
+};
