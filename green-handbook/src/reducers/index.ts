c8fd290ebe6da216dce77876efb7d677e5b5ad5b
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-07-18 07:34:43 +0900

    Update reducers to use combineReducer

--- green-handbook/src/reducers/index.ts
+++ green-handbook/src/reducers/index.ts
@@ -1,42 +1,13 @@
-// eslint-disable-next-line node/no-missing-import, node/no-extraneous-import
-import { ResourceShape } from 'publisher';
-
-declare var window: any;
+import { combineReducers } from 'redux';
+import { reducer as resourcesReducer, State as ResourcesState } from './resources';
+import { reducer as UIReducer, State as UIState } from './ui';
 
 export interface State {
-  resources: {
-    [key: string]: ResourceShape;
-  };
-  ui: {
-    locale: string;
-  };
+  resources: ResourcesState;
+  ui: UIState;
 }
 
-export const initialState: State = {
-  resources: {},
-  ui: {
-    locale: 'en',
-  },
-};
-
-export function reducer(state = initialState, action: any) {
-  const newState = { ...state };
-  const payload = action.payload;
-
-  switch (action.type) {
-    case 'SET_RESOURCE': {
-      const resource = payload.resource;
-      newState.resources[resource.key || resource.id] = resource;
-      break;
-    }
-    case 'CHANGE_LOCALE': {
-      newState.ui.locale = payload.locale;
-      break;
-    }
-    default:
-  }
-  console.log('state', state);
-  console.log('action', action);
-  console.log('newState', newState);
-  return newState;
-}
+export const reducer = combineReducers({
+  resources: resourcesReducer,
+  ui: UIReducer,
+});
