1a5ff33d0125c3acb04d82c63fb2b8388613f03f
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-07-19 08:37:18 +0900

    Update usecases

--- green-handbook/src/usecases/index.ts
+++ green-handbook/src/usecases/index.ts
@@ -1,14 +1,24 @@
 import { Action } from 'redux';
 import { ThunkDispatch } from 'redux-thunk';
 
-import { setResource } from '../actions';
+import { setResource, changeIsFetchingResource, resetResource } from '../actions/resources';
+import { changeLocale } from '../actions/ui';
 import { Resource } from '../services';
 
 export const fetchResource = (key: string) => {
   return (dispatch: ThunkDispatch<{}, {}, Action>, getState) => {
     const state = getState();
+    dispatch(changeIsFetchingResource(key, true));
     return Resource.find(key, state.ui.locale).then(resource => {
       dispatch(setResource(resource));
+      dispatch(changeIsFetchingResource(key, false));
     });
+  };
+};
+
+export const moveTo = (locale: string) => {
+  return (dispatch: ThunkDispatch<{}, {}, Action>) => {
+    dispatch(resetResource());
+    dispatch(changeLocale(locale));
   };
 };
