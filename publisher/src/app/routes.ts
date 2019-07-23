4d99b3f398239cf665e6d67b57300ee146b162e7
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-07-24 08:46:01 +0900

    Format app/routes

--- publisher/src/app/routes.ts
+++ publisher/src/app/routes.ts
@@ -7,5 +7,4 @@ import * as renderer from './server/renderer';
 const STATIC_PATH = path.join(__dirname, 'public');
 
 export const appRouter: express.Application = express();
-appRouter.use('/public', express.static(STATIC_PATH));
-appRouter.get('*', renderer.get);
+appRouter.use('/public', express.static(STATIC_PATH)).get('*', renderer.get);
