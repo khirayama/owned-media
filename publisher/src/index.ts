7f9122b132e07667f978fcdc929f82a0a4c559b3
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-07-26 07:35:39 +0900

    Create src/index file

---
+++ publisher/src/index.ts
@@ -0,0 +1,15 @@
+export { appRouter, Resource } from './app';
+
+export {
+  fetchResources,
+  fetchResource,
+  createResource,
+  updateResource,
+  deleteResource,
+  fetchRelatedResources,
+  createRelations,
+  deleteRelations,
+  apiRouter,
+  adminRouter,
+  initialize,
+} from './api';
