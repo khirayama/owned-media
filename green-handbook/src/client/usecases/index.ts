17c4682ef07d0aa9e0ab6930e2bbeca0168b62bc
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-07-29 07:45:03 +0900

    Define initializer as usecases

--- green-handbook/src/client/usecases/index.ts
+++ green-handbook/src/client/usecases/index.ts
@@ -1,17 +1,29 @@
 import { Action } from 'redux';
 import { ThunkDispatch } from 'redux-thunk';
 
+import { Config, ResourceShape } from 'publisher';
+
 import { setResource, changeIsFetchingResource, resetResource } from '../actions/resources';
 import { changeLocale } from '../actions/ui';
+import { setConfig } from '../actions/config';
 import { State } from '../reducers';
 import { Resource } from '../services';
+
+export const initializePage = (locale: string, config: Config) => {
+  return (dispatch: ThunkDispatch<State, {}, Action>) => {
+    return new Promise(resolve => resolve()).then(() => {
+      dispatch(setConfig(config));
+      dispatch(changeLocale(locale));
+    });
+  };
+};
 
 export const fetchResource = (key: string) => {
   return (dispatch: ThunkDispatch<{}, {}, Action>, getState: () => State) => {
     const state = getState();
     dispatch(changeIsFetchingResource(key, true));
-    return Resource.find(key, state.ui.locale).then(resource => {
-      dispatch(setResource(resource));
+    return Resource.find(key, { locale: state.ui.locale }).then(resource => {
+      dispatch(setResource(resource as ResourceShape));
       dispatch(changeIsFetchingResource(key, false));
     });
   };
