fa0fec0a596b2b450372fc3ecb85eaa6db2c49ec
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-07-28 16:28:46 +0900

    Fix swtich case bug

--- green-handbook/src/server/runServer.ts
+++ green-handbook/src/server/runServer.ts
@@ -33,9 +33,11 @@ export function runServer() {
       case 'EACCES':
         console.error(`${bind} requires elevated privileges`);
         process.exit(1);
+        break;
       case 'EADDRINUSE':
         console.error(`${bind} is already in use`);
         process.exit(1);
+        break;
       default:
         throw err;
     }
