e1f07a3b0dfd7de9eeb3542e7a5d069711289b30
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-07-29 07:44:34 +0900

    Update client/services

--- green-handbook/src/client/services/index.ts
+++ green-handbook/src/client/services/index.ts
@@ -1,4 +1,6 @@
-import axios, { AxiosResponse } from 'axios';
+import axios from 'axios';
+
+import { ResourceShape, ResourceWithAllLocalesShape, Config } from 'publisher';
 
 const PORT = process.env.PORT || 3000;
 
@@ -10,9 +12,48 @@ const req =
       });
 
 export class Resource {
-  public static find(key: string, locale: string) {
-    return req.get(`/${locale}/resources/${key}.json`).then((res: AxiosResponse) => {
-      return res.data;
+  public static baseURL: string = '/api';
+
+  public static fetchConfig() {
+    return new Promise((resolve: (res: Config) => void) => {
+      const url = `${this.baseURL}/config`;
+
+      req.get(url).then(res => {
+        resolve(res.data);
+      });
+    });
+  }
+
+  public static fetch(options?: { locale: string }) {
+    return new Promise((resolve: (res: (ResourceShape | ResourceWithAllLocalesShape)[]) => void) => {
+      const locale = options ? options.locale : null;
+      const url = `${this.baseURL}/resources` + (locale ? `?locale=${locale}` : '');
+
+      req.get(url).then(res => {
+        resolve(res.data);
+      });
+    });
+  }
+
+  public static find(resourceId: string, options?: { locale: string }) {
+    const locale = options ? options.locale : null;
+    const url = `${this.baseURL}/resources/${resourceId}` + (locale ? `?locale=${locale}` : '');
+
+    return new Promise((resolve: (res: ResourceShape | ResourceWithAllLocalesShape) => void) => {
+      req.get(url).then(res => {
+        resolve(res.data);
+      });
+    });
+  }
+
+  public static fetchRelations(resourceId: string, options?: { locale: string }) {
+    const locale = options ? options.locale : null;
+    const url = `${this.baseURL}/resources/${resourceId}/relations` + (locale ? `?locale=${locale}` : '');
+
+    return new Promise((resolve: (res: (ResourceShape | ResourceWithAllLocalesShape)[]) => void) => {
+      req.get(url).then(res => {
+        resolve(res.data);
+      });
     });
   }
 }
