94811fc2740b60715a793d88f892da27835424c8
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-07-29 07:44:09 +0900

    Add a config field to redux.state

--- green-handbook/src/client/reducers/index.ts
+++ green-handbook/src/client/reducers/index.ts
@@ -1,13 +1,19 @@
 import { combineReducers } from 'redux';
+
+import { Config } from 'publisher';
+
+import { reducer as configReducer } from './config';
 import { reducer as resourcesReducer, State as ResourcesState } from './resources';
 import { reducer as UIReducer, State as UIState } from './ui';
 
 export interface State {
+  config: Config | null;
   resources: ResourcesState;
   ui: UIState;
 }
 
 export const reducer = combineReducers({
+  config: configReducer,
   resources: resourcesReducer,
   ui: UIReducer,
 });
