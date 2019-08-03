363309134dbc72c4527f593f5e3a073688c00a46
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-08-04 08:43:53 +0900

    Stop to use locale bar

--- green-handbook/src/client/presentations/components/Navigation.tsx
+++ green-handbook/src/client/presentations/components/Navigation.tsx
@@ -1,12 +1,9 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
 
-import { LocaleBar } from '../../containers/LocaleBar';
-
 export function Navigation() {
   return (
     <nav>
-      <LocaleBar />
       <ul>
         <li>
           <Link to="/ja">日本語</Link>
