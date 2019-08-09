bae16c3b87f1ee0c0210700902b07b8f0df77283
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-08-10 08:03:25 +0900

    Create Page container for app

---
+++ yuggdrasil/src/app/client/containers/Page.tsx
@@ -0,0 +1,49 @@
+import { ThunkDispatch } from 'redux-thunk';
+import { Action } from 'redux';
+import { connect } from 'react-redux';
+import { withRouter, RouteComponentProps, matchPath } from 'react-router-dom';
+
+import { State } from '../reducers';
+import { changeLocale, setPathname } from '../actions';
+import { Page as Component, Props as PageProps, FormattedMessage } from '../presentations/components/Page';
+
+export interface Props extends RouteComponentProps<{ locale?: string }> {
+  title: FormattedMessage;
+  description: FormattedMessage;
+}
+
+const mapStateToProps = (state: State, props: Props) => {
+  const defaultLocale = 'en';
+  const locale = props.match.params.locale || defaultLocale;
+
+  let pathname = '/';
+  const res = matchPath(props.location.pathname, props.match);
+  if (res) {
+    const regexp = new RegExp(`^/${locale}`);
+    pathname = res.url.replace(regexp, '') || '/';
+  }
+
+  return {
+    locale,
+    pathname,
+    title: props.title,
+    description: props.description,
+  };
+};
+
+const mapDispatchToProps = (dispatch: ThunkDispatch<{}, {}, Action>) => {
+  return {
+    onUpdate: (props: PageProps) => {
+      dispatch(changeLocale(props.locale));
+      dispatch(setPathname(props.pathname));
+    },
+  };
+};
+
+// FYI: URLに紐づいた処理はcontainer/Pageで行うが、URLに紐づいた表示（タイトルなど）はcomponents/Pageで行う
+export const Page = withRouter(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps,
+  )(Component),
+);
