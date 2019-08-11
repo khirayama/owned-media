9e2f7808d9869e51d0b13e31a89334e20979f4f2
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-08-11 14:05:19 +0900

    Update test cases for Resource.update

--- yuggdrasil/src/api/models/Resource.test.ts
+++ yuggdrasil/src/api/models/Resource.test.ts
@@ -2,6 +2,10 @@ import { Resource } from './Resource';
 
 Resource.init();
 
+afterAll(() => {
+  Resource.reset();
+});
+
 describe('Resource.create', () => {
   beforeEach(() => {
     Resource.reset();
@@ -19,20 +23,22 @@ describe('Resource.create', () => {
         sample: '1',
       },
     });
-    expect(resource).toEqual({
-      id: '1',
-      type: 'note',
-      key: 'first-content',
-      contents: {
-        name: 'リソース名前',
-        body: 'リソースボディ',
-      },
-      attributes: {
-        sample: '1',
-      },
-      createdAt: resource.createdAt,
-      updatedAt: resource.updatedAt,
-    });
+    if (resource) {
+      expect(resource).toEqual({
+        id: '1',
+        type: 'note',
+        key: 'first-content',
+        contents: {
+          name: 'リソース名前',
+          body: 'リソースボディ',
+        },
+        attributes: {
+          sample: '1',
+        },
+        createdAt: resource.createdAt,
+        updatedAt: resource.updatedAt,
+      });
+    }
     expect(Resource['records']['resourceContents'][0].locale).toEqual('ja_JP');
     expect(Resource['records']['resourceContents'][1].locale).toEqual('ja_JP');
   });
@@ -52,20 +58,22 @@ describe('Resource.create', () => {
       },
       { locale: 'en_US' },
     );
-    expect(resource).toEqual({
-      id: '1',
-      type: 'note',
-      key: 'first-content',
-      contents: {
-        name: 'Resource Name',
-        body: 'Resource Body',
-      },
-      attributes: {
-        sample: '1',
-      },
-      createdAt: resource.createdAt,
-      updatedAt: resource.updatedAt,
-    });
+    if (resource) {
+      expect(resource).toEqual({
+        id: '1',
+        type: 'note',
+        key: 'first-content',
+        contents: {
+          name: 'Resource Name',
+          body: 'Resource Body',
+        },
+        attributes: {
+          sample: '1',
+        },
+        createdAt: resource.createdAt,
+        updatedAt: resource.updatedAt,
+      });
+    }
     expect(Resource['records']['resourceContents'][0].locale).toEqual('en_US');
     expect(Resource['records']['resourceContents'][1].locale).toEqual('en_US');
   });
@@ -142,5 +150,118 @@ describe('Resource.create', () => {
         },
       });
     }).toThrow();
+  });
+});
+
+describe('Resource.update', () => {
+  beforeEach(() => {
+    Resource.reset();
+    Resource.create({
+      type: 'note',
+      key: 'first-content',
+      contents: {
+        name: 'リソース名前',
+        body: 'リソースボディ',
+      },
+      attributes: {
+        sample: '1',
+      },
+    });
+  });
+
+  it('Update fileds without locale.', () => {
+    const resource = Resource.update('1', {
+      key: 'updated-first-content',
+      contents: {
+        name: 'リソース名前 2',
+        body: 'リソースボディ 2',
+      },
+      attributes: {
+        sample: '2',
+        sample2: '2',
+      },
+    });
+    expect(resource).toEqual({
+      id: '1',
+      type: 'note',
+      key: 'updated-first-content',
+      contents: {
+        name: 'リソース名前 2',
+        body: 'リソースボディ 2',
+      },
+      attributes: {
+        sample: '2',
+        sample2: '2',
+      },
+      createdAt: resource.createdAt,
+      updatedAt: resource.updatedAt,
+    });
+  });
+
+  it('Update fileds with locale not existing resource on the locale.', () => {
+    const resource = Resource.update(
+      '1',
+      {
+        key: 'updated-first-content',
+        contents: {
+          name: 'Resource Name',
+          body: 'Resource Body',
+        },
+        attributes: {
+          sample: '1',
+        },
+      },
+      { locale: 'en_US' },
+    );
+    expect(resource).toEqual({
+      id: '1',
+      type: 'note',
+      key: 'updated-first-content',
+      contents: {
+        name: 'Resource Name',
+        body: 'Resource Body',
+      },
+      attributes: {
+        sample: '1',
+      },
+      createdAt: resource.createdAt,
+      updatedAt: resource.updatedAt,
+    });
+    expect(Resource.find({ id: resource.id })[0]).toEqual({
+      id: '1',
+      type: 'note',
+      key: 'updated-first-content',
+      contents: {
+        name: 'リソース名前',
+        body: 'リソースボディ',
+      },
+      attributes: {
+        sample: '1',
+      },
+      createdAt: resource.createdAt,
+      updatedAt: resource.updatedAt,
+    });
+  });
+
+  it('Remove fields.', () => {
+    const resource = Resource.update('1', {
+      contents: {
+        name: null,
+      },
+      attributes: {
+        sample: null,
+      },
+    });
+    expect(resource).toEqual({
+      id: '1',
+      type: 'note',
+      key: 'first-content',
+      contents: {
+        body: 'リソースボディ',
+      },
+      attributes: {},
+      createdAt: resource.createdAt,
+      updatedAt: resource.updatedAt,
+    });
   });
 });
