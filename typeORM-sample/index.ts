e62c4e87bf223f03f4a43afbf7609cee69953952
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-08-28 08:09:09 +0900

    Update index.ts

--- typeORM-sample/index.ts
+++ typeORM-sample/index.ts
@@ -2,11 +2,9 @@ import * as path from 'path';
 
 import * as typeorm from "typeorm";
 
+import { reservedResourceFields } from './var';
 import { supportLocales, resourceTypes } from './config';
 import { Resource } from './src/entity/Resource';
-import { ResourceContent } from './src/entity/ResourceContent';
-import { ResourceMeta } from './src/entity/ResourceMeta';
-import { ResourceAttribute } from './src/entity/ResourceAttribute';
 
 type ResourceResponse = {
   id: string;
@@ -30,27 +28,6 @@ type ResourceResponse = {
   createdAt: Date;
   updatedAt: Date;
 };
-
-type ResourceCreateParams = {
-  key: string;
-  type?: string;
-  name?: string;
-  body?: string;
-  [key: string]: any; // For attributes
-};
-
-const reservedResourceFields = [
-  'id',
-  'key',
-  'type',
-  'locale',
-  'name',
-  'body',
-  'meta',
-  'media',
-  'createdAt',
-  'updatedAt',
-];
 
 function generateResourceResponse(resource: Resource): ResourceResponse {
   if (resource.contents.length !== 1) {
@@ -87,38 +64,6 @@ function generateResourceResponse(resource: Resource): ResourceResponse {
   }
 
   return resourceResponse;
-}
-
-async function createResource(locale: string, params: ResourceCreateParams) {
-  const connection = typeorm.getConnection();
-
-  const key = params.key;
-
-  const content = new ResourceContent();
-  content.locale = locale;
-  content.name = params.name;
-  // TODO: Make markdown file with params.body
-  content.body = `/contents/${key}-${locale}.md`;
-  await connection.manager.save(content);
-
-  const resource = new Resource();
-  resource.key = key;
-  resource.contents = [content];
-  resource.type = params.type ? params.type : resourceTypes[0].name;
-  await connection.manager.save(resource);
-
-  const attributeKeys = Object.keys(params);
-  for (const attributeKey of attributeKeys) {
-    if (reservedResourceFields.indexOf(attributeKey) === -1) {
-      const resourceAttribute = new ResourceAttribute();
-      resourceAttribute.key = attributeKey;
-      resourceAttribute.value = params[attributeKey];
-      resourceAttribute.type = typeof params[attributeKey];
-      await connection.manager.save(resourceAttribute);
-      resource.attributes.push(resourceAttribute);
-      await connection.manager.save(resource);
-    }
-  }
 }
 
 const root = path.resolve(__dirname);
