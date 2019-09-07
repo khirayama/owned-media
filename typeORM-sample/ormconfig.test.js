086cf4ac91aa902fe7095032d13c6d12e6f40ca6
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-09-07 19:36:22 +0900

    Update ormconfig and create ormconfig for test

---
+++ typeORM-sample/ormconfig.test.js
@@ -0,0 +1,14 @@
+module.exports = {
+  type: 'sqlite',
+  database: './data/database.test.sqlite',
+  synchronize: true,
+  logging: true,
+  entities: ['src/entity/**/*.ts'],
+  migrations: ['src/migration/**/*.ts'],
+  subscribers: ['src/subscriber/**/*.ts'],
+  cli: {
+    entitiesDir: 'src/entity',
+    migrationsDir: 'src/migration',
+    subscribersDir: 'src/subscriber',
+  },
+};
