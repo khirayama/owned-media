4a838568dbd3cb2b6cb7dedf19ed4c696324c030
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-09-12 12:30:44 +0900

    Update test cases

--- typeORM-sample/src/sample/usecases.test.ts
+++ typeORM-sample/src/sample/usecases.test.ts
@@ -38,15 +38,53 @@ describe('createResource', () => {
       updatedAt: resource.updatedAt,
     });
   });
-  test('Create resource with unvalid key.', async () => {
+  test('Try to create resource with unvalid key.', async () => {
     const createResourcePromise = createResource({
       key: 'sample_resource',
       type: 'note',
     });
     await expect(createResourcePromise).rejects.toThrow();
   });
+  // await test('Create resource with contents, meta and attributes.', async () => {
+  //   const resource = await createResource({
+  //     key: 'sample-resource-with-contents',
+  //     type: 'note',
+  //     contents: {
+  //       name: 'Sample Resource',
+  //       body: 'Sample Body',
+  //     },
+  //     meta: {
+  //       title: 'title',
+  //       description: 'description',
+  //       keywords: 'keyword1,keyword2',
+  //     },
+  //     attributes: {
+  //       sampleAttribute1: 'sample value 1',
+  //       sampleAttribute2: 'sample value 2',
+  //     },
+  //   });
+  //   expect(resource).toEqual({
+  //     id: resource.id,
+  //     key: 'sample-resource',
+  //     type: 'note',
+  //     contents: {
+  //       name: 'Sample Resource',
+  //       body: 'Sample Body',
+  //     },
+  //     meta: {
+  //       title: 'title',
+  //       description: 'description',
+  //       keywords: 'keyword1,keyword2',
+  //     },
+  //     attributes: {
+  //       sampleAttribute1: 'sample value 1',
+  //       sampleAttribute2: 'sample value 2',
+  //     },
+  //     createdAt: resource.createdAt,
+  //     updatedAt: resource.updatedAt,
+  //   });
+  // });
   test('Create resource with locale.', async () => {});
-  test('Create resource with contents, meta and attributes.', async () => {});
   test('Create resource with unsupported resource type.', async () => {});
   test('Create resource with unsupported locale.', async () => {});
   test('Create resource with same locale and key.', async () => {});
