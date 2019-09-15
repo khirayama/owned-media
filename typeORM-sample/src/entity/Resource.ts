cbdf5ea9f905a315491a09be618a59d56c3406e0
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-09-15 22:24:22 +0900

    Add validation to entity

--- typeORM-sample/src/entity/Resource.ts
+++ typeORM-sample/src/entity/Resource.ts
@@ -1,8 +1,13 @@
 import * as typeorm from 'typeorm';
+import * as classValidator from 'class-validator';
 
+import { resourceTypes } from '../../config';
 import { ResourceContent } from './ResourceContent';
 import { ResourceMeta } from './ResourceMeta';
 import { ResourceAttribute } from './ResourceAttribute';
+
+const regexpString = resourceTypes.map(resourceType => `^${resourceType.name}$`).join('|');
+const regexp = new RegExp(regexpString);
 
 @typeorm.Entity('resources')
 export class Resource {
@@ -10,9 +15,13 @@ export class Resource {
   id: string;
 
   @typeorm.Column({ unique: true })
+  @classValidator.IsNotEmpty()
+  @classValidator.Matches(/^[a-z0-9|-]+$/, { message: 'This resource key($value) is invalid key' })
   key: string;
 
   @typeorm.Column({ type: 'varchar' })
+  @classValidator.IsNotEmpty()
+  @classValidator.Matches(regexp, { message: 'This resource type($value) does not be supportted.' })
   type: string;
 
   @typeorm.OneToMany(() => ResourceContent, resourceContent => resourceContent.resource)
