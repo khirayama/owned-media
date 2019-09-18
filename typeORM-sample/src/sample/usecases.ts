8b75fa9d67829e639ed339e1ca2e3b492154d9a9
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-09-18 09:02:50 +0900

    Update usecases

--- typeORM-sample/src/sample/usecases.ts
+++ typeORM-sample/src/sample/usecases.ts
@@ -10,28 +10,8 @@ export type ResourceCreateParams = {
 };
 
 export async function createResource(params: ResourceCreateParams): Promise<Resource> {
-  const connection = await typeorm.getConnection();
-  const resourceRepository = await connection.getRepository(Resource);
-
   const key = params.key;
   const resourceType = params.type ? params.type : resourceTypes[0].name;
-
-  // Validations
-  const resourceTypeNames = resourceTypes.map(resourceType => resourceType.name);
-  const isNotSupportedResourceType = resourceTypeNames.indexOf(resourceType) === -1;
-  if (isNotSupportedResourceType) {
-    throw new Error(`Not support ${resourceType} as resource type.`);
-  }
-
-  const isExistingResourceWithSameKey = !!(await resourceRepository
-    .createQueryBuilder('resource')
-    .select('COUNT(`id`)')
-    .where('resource.key = :key')
-    .setParameters({ key })
-    .getCount());
-  if (isExistingResourceWithSameKey) {
-    throw new Error(`Existing resource with ${key} as key. If new resource is wanted to create, please change key.`);
-  }
 
   const resource = new Resource();
   resource.key = key;
@@ -43,10 +23,10 @@ export async function createResource(params: ResourceCreateParams): Promise<Reso
       throw new Error(err.constraints.matches);
     }
   } else {
+    const connection = await typeorm.getConnection();
     await connection.manager.save(resource);
+    return resource;
   }
-
-  return resource;
 }
 
 export type ResourceUpdateParams = {
