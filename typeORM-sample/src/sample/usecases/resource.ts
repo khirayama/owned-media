5dbdb35e0f00dc68a39b97cdf7789f3137c04aca
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-09-20 08:35:37 +0900

    Remove needless lines

--- typeORM-sample/src/sample/usecases/resource.ts
+++ typeORM-sample/src/sample/usecases/resource.ts
@@ -1,27 +1,8 @@
 import * as typeorm from 'typeorm';
-import * as classValidator from 'class-validator';
 
 import { resourceTypes } from '../../../config';
 import { Resource } from '../../entity/Resource';
-
-function partialAssign(obj, params) {
-  for (const key of Object.keys(params)) {
-    obj[key] = params[key] !== undefined ? params[key] : obj[key];
-  }
-}
-
-async function save(obj) {
-  const errors = await classValidator.validate(obj);
-  if (errors.length) {
-    for (const err of errors) {
-      throw new Error(err.constraints.matches);
-    }
-  } else {
-    const connection = await typeorm.getConnection();
-    await connection.manager.save(obj);
-    return obj;
-  }
-}
+import { partialAssign, save } from './utils';
 
 export type ResourceCreateParams = {
   key: string;
