9c5951fd0ba54ece3a6906eb1c9aef5b2e9558a2
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-09-26 07:05:38 +0900

    Implement update handler for resource

--- typeORM-sample/src/handlers/resourceHandlers.ts
+++ typeORM-sample/src/handlers/resourceHandlers.ts
@@ -32,4 +32,34 @@ export async function createResourceHandler(req: express.Request, res: express.R
   res.json(resource);
 }
 
-export async function updateResourceHandler(req: express.Request, res: express.Response) {}
+export async function updateResourceHandler(req: express.Request, res: express.Response) {
+  const resourceId = req.params.id;
+  const body = req.body;
+
+  let resource = await resourceRepository.findOne(resourceId);
+
+  if (!resource) {
+    return res.status(404).json();
+  }
+
+  resource.key = body.key !== undefined ? body.key : resource.key;
+  resource.type = body.type !== undefined ? body.type : resource.type;
+  // TODO: Update content, attributes, meta
+
+  const errors = await classValidator.validate(resource);
+  if (errors.length) {
+    res.status(400).json({
+      message: 'Validation Failed',
+      errors: errors.map(err => {
+        return {
+          field: err.constraints,
+          // TODO: message
+        };
+      }),
+    });
+  } else {
+    await resourceRepository.save(resource);
+  }
+
+  res.json(resource);
+}
