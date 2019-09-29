2b12f93a16d81f8d2d288115eb7f83f34f81ca6a
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-09-29 17:16:43 +0900

    Update routing

--- typeORM-sample/src/index.ts
+++ typeORM-sample/src/index.ts
@@ -3,6 +3,11 @@ import { createConnection } from 'typeorm';
 import * as express from 'express';
 
 import { createResourceHandler, updateResourceHandler, deleteResourceHandler } from './handlers/resourceHandlers';
+import {
+  addResourceContentHandler,
+  updateResourceContentHandler,
+  deleteResourceContentHandler,
+} from './handlers/resourceContentHandlers';
 
 const app = express();
 app.use(express.json());
@@ -11,8 +16,11 @@ app.use(express.urlencoded({ extended: true }));
 const resourceRouter = new express.Router();
 resourceRouter
   .post('/', createResourceHandler)
-  .patch('/:id', updateResourceHandler)
-  .delete('/:id', deleteResourceHandler);
+  .patch('/:resourceId', updateResourceHandler)
+  .delete('/:resourceId', deleteResourceHandler)
+  .post('/:resourceId/contents', addResourceContentHandler)
+  .patch('/:resourceId/contents/:contentId', updateResourceContentHandler)
+  .delete('/:resourceId/contents/:contentId', deleteResourceContentHandler);
 
 app.use('/resources', resourceRouter);
 
